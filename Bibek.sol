a714a91817cc399313e2c4ffdd43f2f53dce3b5c
Bibek Sarkar <sarkarbibek379@gmail.com>
2025-10-18 13:32:03 +0530

    Implement IssueFactory and Issue contracts
    This contract allows users to create issues and emit events with issue details.

---
+++ Bibek.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+contract IssueFactory {
+    address[] public Issueaddress;
+    event saveIssue(
+        string title,
+        string disc,
+        string img,
+        address indexed owner,
+        address indexed issueaddress,
+        uint256 time
+    );
+
+    function CreateIssue(
+        string memory _title,
+        string memory _disc,
+        string memory _img
+    ) public {
+        Issue newIssuse = new Issue(_title, _disc, _img, msg.sender);
+        Issueaddress.push(address(newIssuse));
+        emit saveIssue(
+            _title,
+            _disc,
+            _img,
+            msg.sender,
+            address(newIssuse),
+            block.timestamp
+        );
+    }
+}
+contract Issue {
+    string public I_title;
+    string public I_disc;
+    string public I_img;
+    address public Owner;
+    uint256 public Vote;
+    event Showvote(uint256 Votevalue, address Voter, uint256 time);
